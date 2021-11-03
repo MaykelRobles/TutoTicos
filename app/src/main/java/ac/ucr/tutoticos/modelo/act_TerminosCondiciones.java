08ea5e8d3135efd2d6e91100f29d7d7909d912e5
MaykelRobles <MAIKEL.ROBLES@ucr.ac.cr>
2021-11-03 14:30:42 -0600

    Pantalla de términos y condiciones
    Creación del lyt_términos_condiciones

---
+++ app/src/main/java/ac/ucr/tutoticos/modelo/act_TerminosCondiciones.java
@@ -0,0 +1,16 @@
+package ac.ucr.tutoticos.modelo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ac.ucr.tutoticos.R;
+
+public class act_TerminosCondiciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_terminos_condiciones);
+    }
+}
